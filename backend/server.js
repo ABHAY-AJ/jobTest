d67bd255a8e67b2137f038a65aa368b08768c3ea
Abhay <krabhay476@gmail.com>
2024-09-13 03:54:48 +0530

    kk

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require("./config/db")
 require('dotenv').config();
 const cors = require("cors");
+const path = require('path');
 const app = express();
 
 
